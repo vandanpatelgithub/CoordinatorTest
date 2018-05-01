24617516cf9c7528483e9a3d832ae9eda7e9136e
Vandan Patel <vandan.patel@nike.com>
2018-04-30 21:17:12 -0700

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Patel, Vandan (ETW - FLEX) on 4/30/18.
+//  Copyright © 2018 Patel, Vandan (ETW - FLEX). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
